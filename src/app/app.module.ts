ddbddcc11957779c2cb64c1d778a169b81207aa4
sdonose <sdonose@pentalog.fr>
2017-07-27 14:26:50 +0300

    angular2 material settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule} from '@angular/material';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
