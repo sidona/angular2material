2d9def39117c6f6021d22e3596e701a603d900f6
sdonose <sdonose@pentalog.fr>
2017-07-28 15:25:54 +0300

    angular2 material sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MdButtonModule} from '@angular/material';
+import {MdButtonModule, MdSidenavModule, MdDialogModule, MdMenuModule, MdToolbarModule} from '@angular/material';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { DialogComponent } from './dialog/dialog.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MdUniqueSelectionDispatcher} from "@angular2-material/core";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent,
+    NavbarComponent
   ],
+  entryComponents: [DialogComponent],
   imports: [
     BrowserModule,
     HttpModule,
     BrowserAnimationsModule,
     MdButtonModule,
+    MdSidenavModule,
+    MdDialogModule,
+    MdMenuModule,
+    MdToolbarModule
   ],
-  providers: [],
+  providers: [MdUniqueSelectionDispatcher],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
